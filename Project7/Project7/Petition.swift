bdc64b4bb29f81f69f5be5b9c7ca90e92ccc76ee
Chris Weirup <cweirup@gmail.com>
2019-04-03 14:21:21 -0400

    Added Project7 (Days 33-34)

---
+++ Project7/Project7/Petition.swift
@@ -0,0 +1,15 @@
+//
+//  Petition.swift
+//  Project7
+//
+//  Created by Weirup, Chris on 2019-04-03.
+//  Copyright © 2019 Christopher Weirup. All rights reserved.
+//
+
+import Foundation
+
+struct Petition: Codable {
+    var title: String
+    var body: String
+    var signatureCount: Int
+}
