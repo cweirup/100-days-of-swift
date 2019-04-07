2b03c4c113ba07869cc80872322dc74d798cdd29
Chris Weirup <cweirup@gmail.com>
2019-04-06 20:51:27 -0400

    Day 40 - GCD with Project 1

---
+++ Project1-Day40 Challenge/Project1/ViewController.swift
@@ -0,0 +1,70 @@
+//
+//  ViewController.swift
+//  Project1
+//
+//  Created by Christopher Weirup on 2019-03-25.
+//  Copyright © 2019 Christopher Weirup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "Storm Viewer"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .action, target: self, action: #selector(shareTapped))
+        
+        performSelector(inBackground: #selector(loadImages), with: nil)
+        
+    }
+    
+    @objc func loadImages () {
+        let fm = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fm.contentsOfDirectory(atPath: path).sorted()
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                // this is a picture to load!
+                pictures.append(item)
+            }
+        }
+        
+        tableView.performSelector(onMainThread: #selector(UITableView.reloadData), with: nil, waitUntilDone: false)
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Detail") as? DetailViewController {
+            vc.selectedImage = pictures[indexPath.row]
+            vc.selectedImageNumber = indexPath.row + 1
+            vc.totalImages = pictures.count
+            navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+    @objc func shareTapped() {
+        let message = "You should try this app, it's great!"
+        
+        let vc = UIActivityViewController(activityItems: [message], applicationActivities: [])
+        // Needed for iPad, to show where the popover is called from
+        vc.popoverPresentationController?.barButtonItem = navigationItem.rightBarButtonItem
+        present(vc, animated: true)
+    }
+
+}
+
