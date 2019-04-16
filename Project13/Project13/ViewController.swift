37a63cef87d1f30916d35a018deec19d6e585d29
Chris Weirup <cweirup@gmail.com>
2019-04-15 20:10:00 -0400

    Adds Project13 to repo

---
+++ Project13/Project13/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  Project13
+//
+//  Created by Christopher Weirup on 2019-04-15.
+//  Copyright © 2019 Christopher Weirup. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var imageView: UIImageView!
+    @IBOutlet var intensity: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func changeFilter(_ sender: Any) {
+    }
+    
+    @IBAction func save(_ sender: Any) {
+    }
+    
+    @IBAction func intenstiyChanged(_ sender: Any) {
+    }
+    
+    
+}
+
