d7cc44b977ee6bfd535abfef3a418cbe616fca0c
Chris Weirup <cweirup@gmail.com>
2019-04-26 00:13:54 -0400

    Initial commit of Project17.

---
+++ Project17/Project17/GameScene.swift
@@ -0,0 +1,18 @@
+//
+//  GameScene.swift
+//  Project17
+//
+//  Created by Christopher Weirup on 2019-04-26.
+//  Copyright © 2019 Christopher Weirup. All rights reserved.
+//
+
+import SpriteKit
+
+class GameScene: SKScene {
+    
+    
+    override func didMove(to view: SKView) {
+  
+    }
+    
+}
